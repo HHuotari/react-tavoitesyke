404ea6a7bf2a17719081f5e865af0336e1448708
hhuotari <h.huotari500@gmail.com>
2022-10-02 16:09:25 +0300

    Tavoitesyke valmis koodi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [bpmL, setBpmL] = useState(0);
+  const [bpmU, setBpmU] = useState(0);
+
+  function calculate() {
+    setBpmU((220-age)*0.85)
+    setBpmL((220-age)*0.65)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h3>Heart rate limits calculator</h3>
+
+      <form>
+        <div>
+          <label>Age</label>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)} />
+        </div>
+
+        <div>
+          <label>Heart rate limits </label>
+          <output>{bpmL.toFixed(0)}-{bpmU.toFixed(0)}</output>
+        </div>
+          <button type="button" onClick={calculate}>Calculate</button>
+      </form>
+      
     </div>
-  );
+  )
 }
 
 export default App;
